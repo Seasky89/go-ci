e2a1e8389d551f8f40a38edec6b6f49d4633dee7
Marcel Kinoshita <marcel_hideki@hotmail.com>
2026-02-04 16:35:22 -0300

    correcao do retorno de erro do service da categoria

--- internal/services/categoria.go
+++ internal/services/categoria.go
@@ -11,11 +11,11 @@ import (
 func DecodeAndValidateCategoria(r *http.Request) (*models.Categoria, error) {
 	var categoria models.Categoria
 	if err := json.NewDecoder(r.Body).Decode(&categoria); err != nil {
-		return nil, errors.New("Erro ao decodificar a categoria")
+		return nil, errors.New("erro ao decodificar a categoria")
 	}
 	err := validators.ValidateCategoria(&categoria)
 	if err != nil {
-		return nil, errors.New("Erro de validação: " + err.Error())
+		return nil, errors.New("erro de validação: " + err.Error())
 	}
 	return &categoria, nil
 }
